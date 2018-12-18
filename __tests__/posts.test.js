db0c2c19e478cdcb3b1374d84683e20f4951f1bd
jeovazero <jeovapereira2000@live.com>
2018-12-18 17:41:04 -0300

    update tests

--- __tests__/posts.test.js
+++ __tests__/posts.test.js
@@ -1,21 +1,29 @@
 const api = require('../helpers/api');
 
-describe('[getPosts]', () => {
-  it('should load all posts', async () => {
+describe('[Mutation] addPost', () => {
+  it('should add a post, then load all them', async (done) => {
+    const { data: invokerData, error: invokerError } = await api.addPost({ author: "invoker", text: "My name is Karl!"});
+    expect(invokerError).toBeUndefined();
+
+    const { addPost } = invokerData;
+    expect(addPost.length).toStrictEqual(1);
+
+    const post = addPost[0];
+    expect(post.author).toStrictEqual("invoker");
+    expect(post.text).toStrictEqual("My name is Karl!");
+
+    done();
+  })
+});
+
+describe('[Query] getPosts', () => {
+  it('should load all posts', async (done) => {
     const { data, error } = await api.getPosts();
     expect(error).toBeUndefined();
 
     const { getPosts } = data;
-    expect(getPosts.length).toStrictEqual(3);
-  })
-});
-
-describe('[addPost]', () => {
-  it('should add a post, then load all them', async () => {
-    const { data, error } = await api.addPost({ author: "invoker", text: "My name is Karl!"});
-    expect(error).toBeUndefined();
-    console.log(data)
-    const { addPost } = data;
-    expect(addPost.length).toStrictEqual(4);
+    expect(getPosts.length).toStrictEqual(1);
+    
+    done();
   })
 });
